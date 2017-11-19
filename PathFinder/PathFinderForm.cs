5dff91421e9fe82d7bc930df859548116b1d2e4a
Generalwrex <bjdroot@gmail.com>
2017-11-19 14:31:34 -0800

    Donation link had the wrong email. Added correct one

--- PathFinder/PathFinderForm.cs
+++ PathFinder/PathFinderForm.cs
@@ -60,7 +60,7 @@ namespace PathFinder
 
         private void toolStripButton1_Donate_Click(object sender, EventArgs e)
         {
-            string donateURL = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=generalwrex%40gmail%2ecom" +
+            string donateURL = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=generalwrexdev%40gmail%2ecom" +
                 "&lc=US&item_name=Generalwrex%27s%20Programs&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest";
             System.Diagnostics.Process.Start(donateURL);
         }
